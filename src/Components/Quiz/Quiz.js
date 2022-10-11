730110ff148e74d9246c82c1d8d911c40d42f0ac
noman2047 <nomancse101@gmail.com>
2022-10-12 00:31:35 +0600

    add question and answer

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -4,13 +4,14 @@ import QuizDetails from '../QuizDetails/QuizDetails';
 
 const Quiz = () => {
   const quizQuestions=useLoaderData();
+  const mcqTopic=quizQuestions.data.name;
   const getQuestions=quizQuestions.data.questions;
   return (
     <div>
+      <h3 style={{marginTop:"50px",color:"blueviolet",fontWeight:"bold"}}>Quiz of {mcqTopic}</h3>
       {
         getQuestions.map(getQuestion=><QuizDetails key={getQuestion.id} getQuestion={getQuestion}></QuizDetails>)
       }
-      <h1>This is mcq quiz system.</h1>
     </div>
   );
 };
--- src/Components/QuizDetails/QuizDetails.js
+++ src/Components/QuizDetails/QuizDetails.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import './quiz.css'
 
-const QuizDetails = ({getQuestion}) => {
-  console.log(getQuestion);
+const QuizDetails = (props) => {
+  const {getQuestion}=props;
   const {id,question,options,correctAnswer}=getQuestion;
+
   return (
-    <div>
-      
-      
+    <div className='container'>
+      <div className='questions'>
+        <h4>{question}</h4>
+        <div className='option'>
+          {
+          options.map(option=><p>{option}</p>)
+          }
+        </div>
+      </div>
     </div>
   );
 };
